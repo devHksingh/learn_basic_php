bda1f0750553398f4c30059c33e2f94f3a1854dc
devHksingh <hksingh.dev@gmail.com>
2025-06-06 18:18:59 +0530

    learn php expression variable etc

--- expressions.php
+++ expressions.php
@@ -2,5 +2,25 @@
 // print statement
 echo "welcome to php\n";
 // assign value to variable
+// assignment expression
 $name = "Alice";
+// it will return what is assign the value
+echo $person_2 = "Bob\n";
+// String concentination 
+echo "Hello, " . $name . "!\n";
+// arithmetic expression
+$pizzas = 3;
+$slicePerPizza = 8;
 
+$totalSlices = $pizzas * $slicePerPizza;
+echo $totalSlices . "\n";
+// output is in number formate
+echo $pizzas;
+// Output is in string formate type casting 
+echo "\n" . "Total pizza slices: " . $totalSlices . "\n";
+// boolean
+$isHungry = true;
+
+echo "Hungry?";
+// ternary opreator
+echo $isHungry? "Yes\n":"No\n";
