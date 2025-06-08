580fde154ca0df67779fefcb10f59a58d500c043
devHksingh <hksingh.dev@gmail.com>
2025-06-08 17:02:52 +0530

    add switch statement

---
+++ switch.php
@@ -0,0 +1,17 @@
+<?php
+
+$size ="L";
+switch($size){
+  case "M":
+    echo "Medium size\n";
+    break;
+  case 'S':
+    echo "Small size\n";
+    break;
+  case 'L':
+    echo "Large size\n";
+    break;
+  default:
+  echo "Extra Large size\n";
+  break;
+}
