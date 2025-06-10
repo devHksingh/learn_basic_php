1d74333f07a22096de873f028400ae29e73c7ef8
devHksingh <hksingh.dev@gmail.com>
2025-06-10 11:06:57 +0530

    add anonymous func

---
+++ Basic_Function/anonymous.php
@@ -0,0 +1,39 @@
+<?php
+
+// clouser
+
+$greet = function ($name) {
+  echo "Hello ,$name\n";
+};
+echo $greet("David");
+
+$numbers = [1, 2, 3, 4];
+$squared = array_map(function ($n) {
+  return $n * $n;
+},$numbers);
+
+var_dump($numbers,$squared);
+
+$message = "Heloo!";
+// anonymous functions is don't have access to the variables defined outside.
+/*
+to access outside variables in anonymous function add use statement after a function
+use($message)
+i want this message variable to also exist in the context of this anonymous function
+use ($message)
+copy message variable is only existed in function . it does not change orginal value. 
+use (&$message)
+to modified variable message use "&" sign that return form anonymous fuction 
+*/
+// to modified variable message use "&" that return form anonymous fuction 
+$greet2 = function ($name) use (&$message) {
+  $message = $message . "!";
+  return "$message ,$name\n";
+};
+echo $greet2("David");
+echo "$message\n";
+ $squared = array_map(function($n){
+  return $n*$n;
+},$numbers);
+
+var_dump($numbers,$squared);
--- Basic_Function/variadic.php
+++ Basic_Function/variadic.php
@@ -1,11 +1,13 @@
 <?php
+
 declare(strict_types=1);
-function sum (int ...$numbers):int{
+function sum(int ...$numbers): int
+{
   // array(0) {
-// }
+  // }
   var_dump($numbers);
-  $sum =0;
-  foreach($numbers as $number){
+  $sum = 0;
+  foreach ($numbers as $number) {
     $sum += $number;
   }
   return $sum;
@@ -13,23 +15,24 @@ function sum (int ...$numbers):int{
 // int(0)
 var_dump(sum());
 var_dump(sum(5));
-var_dump(sum(2,10,20,22)); 
+var_dump(sum(2, 10, 20, 22));
 
 $numbers = [1, 2, 3, 4];
 
-sum($numbers[0],$numbers[1],$numbers[2],$numbers[3]);
+sum($numbers[0], $numbers[1], $numbers[2], $numbers[3]);
 
 // unpacking array in arguments
 var_dump(sum(...$numbers));
 
 // another fun;ction 
-function introduceTeam(string $teamName, string ...$members):void{
+function introduceTeam(string $teamName, string ...$members): void
+{
   var_dump($members);
   echo "Team : $teamName\n";
-  echo "Members: ". implode(", ",$members). "\n";
+  echo "Members: " . implode(", ", $members) . "\n";
 }
-introduceTeam("A team","Jhon","Alice");
-$members = ["Harry","Johhny","Joe"];
+introduceTeam("A team", "Jhon", "Alice");
+$members = ["Harry", "Johhny", "Joe"];
 introduceTeam("B team", ...$members);
 
-introduceTeam("C team", "Alice" ,...$members);
+introduceTeam("C team", "Alice", ...$members);
